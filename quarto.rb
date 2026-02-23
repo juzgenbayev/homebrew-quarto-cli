21f4376143492305bad569a9d0bba7c1f7c03d8b
alisherj <45711590+juzgenbayev@users.noreply.github.com>
2026-02-23 11:23:33 -0600

    Upgrade Quarto to version 1.8.27
    Updated Quarto version to 1.8.27 and fixed sha256 URL.

--- quarto.rb
+++ quarto.rb
@@ -1,8 +1,8 @@
 class Quarto < Formula
   desc "Scientific and technical publishing system built on Pandoc"
   homepage "https://www.quarto.org/"
-  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.32/quarto-1.7.32-macos.tar.gz"
-  sha256 "b49912bbe2b507f03d0bac9089f0e97437a87226c59a371e4eff8712557b16e8"
+  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.8.27/quarto-1.8.27-macos.tar.gz"
+  sha256 "https://github.com/quarto-dev/quarto-cli/releases/download/v1.8.27/quarto-1.8.27-macos.tar.gz"
   license "GPL-2.0-or-later"
 
   livecheck do
